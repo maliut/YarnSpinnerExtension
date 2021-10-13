f5bbae62c27788d95934768d85344e6a4d9b1e43
maliut <lqn619@163.com>
2021-10-14 00:02:59 +0800

    add save and load support

---
+++ Assets/Samples/Yarn Spinner/1.2.7/Space/Scripts/SaveLoadManager.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace Yarn.Unity.Example
+{
+    public class SaveLoadManager : MonoBehaviour
+    {
+
+        private const BindingFlags PublicFlag = BindingFlags.Public | BindingFlags.Instance;
+        private const BindingFlags PrivateFlag = BindingFlags.NonPublic | BindingFlags.Instance;
+
+        private DialogueRunner runner;
+        private Dialogue dialogue => runner.Dialogue;
+
+        private void Start()
+        {
+            runner = FindObjectOfType<DialogueRunner>();
+        }
+
+        private void OnGUI()
+        {
+            // 点击此按钮保存当前的对话状态
+            if (GUI.Button(new Rect(0, 0, 200, 100), "save")) {
+                // 这里使用自带的 JsonUtility 进行序列化，实际可以选用其他方式进行序列化
+                var json = JsonUtility.ToJson(currentState);
+                // Debug.Log("save " + json);
+                // 这里把序列化后的内容保存到 PlayerPrefs 中，实际可以选用其他持久化的方式
+                PlayerPrefs.SetString("saved", json);
+            }
+            // 点击此按钮读取之前保存的对话状态
+            if (GUI.Button(new Rect(0, 100, 200, 100), "load")) {
+                var json = PlayerPrefs.GetString("saved");
+                // Debug.Log("load " + json);
+                var stateWrapper = JsonUtility.FromJson<VmStateWrapper>(json);
+                // 获取当前 vm 的 state
+                var vmFieldInfo = typeof(Dialogue).GetField("vm", PrivateFlag);
+                var vm = vmFieldInfo!.GetValue(dialogue);
+                var vmStateFieldInfo = vm.GetType().GetField("state", PrivateFlag);
+                var vmState = vmStateFieldInfo!.GetValue(vm); // VirtualMachine.State
+                // 用读取出来的 state 覆盖掉当前的 state
+                stateWrapper.Overwrite(vmState);
+                // 还需要设置 vm.currentNode，因为 state 只保存了 currentNodeName，因此还要根据 nodeName 把 currentNode 恢复出来
+                // 注意这里需要确保 program 加载完成。一般都是在启动时加载，所以问题不大。
+                var program = (Program) typeof(Dialogue)
+                    .GetField("_program", PrivateFlag)!.GetValue(dialogue);
+                var currentNode = program.Nodes[stateWrapper.currentNodeName];
+                vm.GetType().GetField("currentNode", PrivateFlag)!.SetValue(vm, currentNode);
+                
+                var dialogueUI = (DialogueUI) runner.dialogueUI;
+                // 重置 dialogUI，否则在选项状态时读档会导致选项没有被隐藏
+                dialogueUI.dialogueContainer.SetActive(false);
+                foreach (var button in dialogueUI.optionButtons) {
+                    button.gameObject.SetActive(false);
+                }
+                // 重置 vm.executionState 为 suspended，否则读档时若处在选项状态会报错
+                // 改成 stopped 应该也行，但是不能直接调用 runner.Stop(), 因为内部会重置 vm.state
+                vm.GetType().GetField("_executionState", PrivateFlag)!.SetValue(vm, 2);
+                // 类似 DialogRunner#StartDialogue, 但是不能调用 Dialogue.SetNode，因为内部会重置 vm.state
+                dialogueUI.StopAllCoroutines();
+                runner.IsDialogueRunning = true;
+                dialogueUI.DialogueStarted();
+                typeof(DialogueRunner).GetMethod("ContinueDialogue", PrivateFlag)!
+                    .Invoke(runner, new object[0]);
+            }
+        }
+
+        private VmStateWrapper currentState;
+
+        /// <summary>
+        /// 在每一行开始的时候获取当前 vm 的 state。注意要把这个方法注册到 dialogRunner 的相应回调中
+        /// 之所以要在这里获取 state，而不是点击保存按钮时再获取，是因为虚拟机的实现在执行完指令后会把 programCounter++
+        /// 这样会导致读取到的状态实际是下一句话
+        /// </summary>
+        public void OnLineStart()
+        {
+            currentState = new VmStateWrapper(GetVmState());
+        }
+        
+        /// <summary>
+        /// 通过反射获取虚拟机当前的状态 vm.state
+        /// </summary>
+        /// <returns>vm.state</returns>
+        private object GetVmState()
+        {
+            var vmFieldInfo = typeof(Dialogue).GetField("vm", PrivateFlag);
+            var vm = vmFieldInfo!.GetValue(dialogue);
+            var vmStateFieldInfo = vm.GetType().GetField("state", PrivateFlag);
+            var vmState = vmStateFieldInfo!.GetValue(vm); // VirtualMachine.State
+            return vmState;
+        }
+
+        /// <summary>
+        /// 包裹 vm.state, 使之变为 JsonUtility 可以序列化的形式
+        /// </summary>
+        [Serializable]
+        private class VmStateWrapper
+        {
+
+            public string currentNodeName;
+            public int programCounter;
+            public SerializableOption[] currentOptions;
+            public SerializableYarnValue[] stackValues;
+            
+            public VmStateWrapper(object rawState)
+            {
+                var vmStateKlass = rawState.GetType(); // VirtualMachine.State
+                currentNodeName = (string) vmStateKlass
+                    .GetField("currentNodeName", PublicFlag)!.GetValue(rawState);
+                programCounter = (int) vmStateKlass
+                    .GetField("programCounter", PublicFlag)!.GetValue(rawState);
+                var rawOptions = (List<KeyValuePair<Line, string>>) vmStateKlass
+                    .GetField("currentOptions", PublicFlag)!.GetValue(rawState);
+                currentOptions = new SerializableOption[rawOptions.Count];
+                for (var i = 0; i < rawOptions.Count; i++) {
+                    currentOptions[i] = new SerializableOption {
+                        ID = rawOptions[i].Key.ID,
+                        Substitutions = rawOptions[i].Key.Substitutions,
+                        Value = rawOptions[i].Value
+                    };
+                }
+                var rawStack = (Stack<Value>) vmStateKlass
+                    .GetField("stack", PrivateFlag)!.GetValue(rawState);
+                var rawArray = rawStack.ToArray();
+                stackValues = new SerializableYarnValue[rawArray.Length];
+                for (var i = 0; i < rawArray.Length; i++) {
+                    stackValues[i] = new SerializableYarnValue {
+                        ValueType = rawArray[i].type,
+                        StringValue = rawArray[i].AsString
+                    };
+                }
+            }
+            
+            /// <summary>
+            /// 把存档里反序列化出来的内容覆盖掉当前的 vm.state
+            /// </summary>
+            public void Overwrite(object rawState)
+            {
+                var vmStateKlass = rawState.GetType();
+                vmStateKlass.GetField("currentNodeName", PublicFlag)!
+                    .SetValue(rawState, currentNodeName);
+                vmStateKlass.GetField("programCounter", PublicFlag)!
+                    .SetValue(rawState, programCounter);
+                var rawOptions = new List<KeyValuePair<Line, string>>(currentOptions.Length);
+                rawOptions.AddRange(currentOptions.Select(option => new KeyValuePair<Line, string>(new Line {
+                    ID = option.ID,
+                    Substitutions = option.Substitutions
+                }, option.Value)));
+                vmStateKlass.GetField("currentOptions", PublicFlag)!
+                    .SetValue(rawState, rawOptions);
+                var rawStack = new Stack<Value>(stackValues.Length);
+                foreach (var value in stackValues) {
+                    switch (value.ValueType) {
+                        case Value.Type.Null:
+                            rawStack.Push(Value.NULL);
+                            break;
+                        case Value.Type.Bool:
+                            rawStack.Push(new Value(bool.Parse(value.StringValue)));
+                            break;
+                        case Value.Type.Number:
+                            rawStack.Push(new Value(float.Parse(value.StringValue)));
+                            break;
+                        case Value.Type.String:
+                            rawStack.Push(new Value(value.StringValue));
+                            break;
+                        // 其他类型在序列化时调用 AsString 已经报错了，因此理论上不会出现
+                    }
+                }
+                vmStateKlass.GetField("stack", PrivateFlag)!
+                    .SetValue(rawState, rawStack);
+            }
+
+            [Serializable]
+            public struct SerializableOption
+            {
+                public string ID;
+                public string[] Substitutions;
+                public string Value;
+            }
+            
+            [Serializable]
+            public struct SerializableYarnValue
+            {
+                public Value.Type ValueType;
+                public string StringValue;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,25 +1,56 @@
-## YarnSpinnerExtension
+## Save & Load
 
-为 [YarnSpinner](https://yarnspinner.dev/) 增加一些新的能力。
+提供对话内存档和读档的能力。
 
-注：基于 YarnSpinner 1.2 版本
+在官方仓库中已经有两个 PR 试图解决这个问题（
+[#65](https://github.com/YarnSpinnerTool/YarnSpinner/pull/65)
+、
+[#242](https://github.com/YarnSpinnerTool/YarnSpinner/pull/242)
+），但并没有合并到主干中，也无法拿来即用。不过它们的思路还是可以借鉴的。
 
-分支说明：
-- `master`: 原始的官方 demo
-- `saveload`: 增加了对话中存档和读档的能力
-- `richtext`: 增加了富文本展示的能力
+本方案则是利用反射，实现无侵入式的修改，避免了对源码的改动，更加便于使用。
+同时对 demo 工程进行了适配，完善了很多细节问题。
 
-具体的文档说明参见各分支。
+所有逻辑都在 `SaveLoadManager.cs` 文件中，并附有注释可以参考。
+
+注意点：
+1. **（重要）为了存读档功能正常，不能使用 Yarn Spinner 自带的 Inline expressions 语法。**
+    这是因为 Yarn Spinner 底层虚拟机没有提供指令级别的状态保存和恢复功能。当调用 `OnLineStart` 
+   保存状态时，Inline expressions 已经被求值。但当调用 `ContinueDialogue` 恢复执行时，
+   Inline expressions 又会被再次求值，导致虚拟机内部的栈错误。
+   因此如需要这项功能，建议自己通过字符串替换等方式实现。
+2. 代码只处理了对话中存档的情况。在真实项目中你还要考虑玩家在非对话状态中存档的情况，
+   此时不应该调用 `ContinueDialogue` 启动对话系统。
+3. 代码只保存了 Yarn Spinner 底层虚拟机的内部状态，对于 VariableStorage 和其他游戏状态你仍需自行保存。
 
 ---
 
-Provide some new abilities for [YarnSpinner](https://yarnspinner.dev/).
+This branch provides save and load support when dialogue running.
 
-Note: it's based on YarnSpinner v1.2 version.
+There are already 2 pull requests for this feature(
+[#65](https://github.com/YarnSpinnerTool/YarnSpinner/pull/65)
+、
+[#242](https://github.com/YarnSpinnerTool/YarnSpinner/pull/242)
+)，but both not merged, cannot be used out of the box. However 
+their ideas can still be used for reference.
 
-branches:
-- `master`: the official demo project.
-- `saveload`: add save and load function when dialogue is running.
-- `richtext`: add rich text support.
+My solution is to achieve non-intrusive modifications by reflection.
+It's easier to use, and fit for the demo project, perfected a lot of details.
 
-Checkout branches to read the detailed documentation. 
+All code logic can be found in `SaveLoadManager.cs`, with detailed comments.
+
+Notice:
+1. **(Important) For save and load function work properly, DO NOT use Yarn Spinner's
+    Inline expressions syntax.** It's because there is lack support in the low-level 
+   virtual machine for instruction-level save and load ability. 
+   When `OnLineStart` is called to save the state, Inline expressions have been already 
+   evaluated. But when `ContinueDialogue` is called to resume the state, 
+   these Inline expressions will be evaluated again, which breaks the stack state in 
+   virtual machine. So if you need this feature, I recommend achieving by yourself, 
+   using string replacement or other ways.
+2. The code only handles situation that users save when dialogue system running. 
+    In real-world projects you should also handle situations when dialogue system not running. 
+   In this case you should not call `ContinueDialogue` to wrongly launch the dialogue system.
+3. The code only save the state of Yarn Spinner's low-level virtual machine. 
+    For VariableStorage and other game states you should save them in your own ways.
+
