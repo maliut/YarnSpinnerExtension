e18387489cbcd83aa66c56299740a3573bb79014
maliut <lqn619@163.com>
2021-10-14 23:14:32 +0800

    add rich text function

---
+++ Assets/Samples/Yarn Spinner/1.2.7/Space/Scripts/TMP_DialogueUI.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+namespace Yarn.Unity.Example
+{
+    public class TMP_DialogueUI : DialogueUI
+    {
+        public TMP_Text dialogueTextUI;
+        
+        // 代替父类的私有变量
+        private bool userRequestedNextLine_Sub = false;
+        
+        /// Runs a line.
+        /// <inheritdoc/>
+        public override Dialogue.HandlerExecutionType RunLine (Yarn.Line line, ILineLocalisationProvider localisationProvider, System.Action onLineComplete)
+        {
+            // Start displaying the line; it will call onComplete later
+            // which will tell the dialogue to continue
+            StartCoroutine(DoRunLine(line, localisationProvider, onLineComplete));
+            return Dialogue.HandlerExecutionType.PauseExecution;
+        }
+
+        /// Show a line of dialogue, gradually        
+        private IEnumerator DoRunLine(Yarn.Line line, ILineLocalisationProvider localisationProvider, System.Action onComplete) {
+            onLineStart?.Invoke();
+
+            userRequestedNextLine_Sub = false;
+            
+            // The final text we'll be showing for this line.
+            string text = localisationProvider.GetLocalisedTextForLine(line);
+
+            if (text == null) {
+                Debug.LogWarning($"Line {line.ID} doesn't have any localised text.");
+                text = line.ID;
+            }
+            
+            // 新增：一句话开始时就把文字全部设置到 TMP_Text 上，但设置可见字符数为 0
+            dialogueTextUI.maxVisibleCharacters = 0;
+            dialogueTextUI.text = text;
+            yield return new WaitForEndOfFrame(); // wait for text to be parsed
+            var charCount = dialogueTextUI.textInfo.characterCount;
+            // Debug.Log(dialogueTextUI.GetParsedText());
+            // Debug.Log(charCount);
+
+            if (textSpeed > 0.0f) {
+                // Display the line one character at a time
+                for (var i = 0; i < charCount; i++) {
+                    dialogueTextUI.maxVisibleCharacters = i + 1;
+                    if (userRequestedNextLine_Sub) {
+                        // We've requested a skip of the entire line.
+                        // Display all of the text immediately.
+                        dialogueTextUI.maxVisibleCharacters = int.MaxValue;
+                        break;
+                    }
+                    yield return new WaitForSeconds (textSpeed);
+                }
+            } else {
+                // Display the entire line immediately if textSpeed <= 0
+                dialogueTextUI.maxVisibleCharacters = int.MaxValue;
+            }
+
+            // We're now waiting for the player to move on to the next line
+            userRequestedNextLine_Sub = false;
+
+            // Indicate to the rest of the game that the line has finished being delivered
+            onLineFinishDisplaying?.Invoke();
+
+            while (userRequestedNextLine_Sub == false) {
+                yield return null;
+            }
+
+            // Avoid skipping lines if textSpeed == 0
+            yield return new WaitForEndOfFrame();
+
+            // Hide the text and prompt
+            onLineEnd?.Invoke();
+
+            onComplete();
+
+        }
+        
+        public void MarkLineComplete_Sub() {
+            userRequestedNextLine_Sub = true;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,25 +1,23 @@
-## YarnSpinnerExtension
+## Rich Text
 
-为 [YarnSpinner](https://yarnspinner.dev/) 增加一些新的能力。
+为 Yarn Spinner 增加富文本展示的功能。
 
-注：基于 YarnSpinner 1.2 版本
+无论是默认的 UI.Text 还是 TextMeshPro 都支持类似 HTML 的富文本标签。但 demo 中使用了字符串分割的方式来实现打字效果，这会破坏富文本的展示。
 
-分支说明：
-- `master`: 原始的官方 demo
-- `saveload`: 增加了对话中存档和读档的能力
-- `richtext`: 增加了富文本展示的能力
+因此我们必须换一个思路：在一句话开始时就把整行文本都设置上去，通过设置可见字符数来实现打字效果。这里我们选用了 TextMeshPro，因为它有现成的 `maxVisibleCharacters` 属性可以使用。
 
-具体的文档说明参见各分支。
+如果你选择默认的 UI.Text，也可以考虑重写 `OnPopulateMesh` 方法来实现同样的效果。
+
+具体实现上，我们定义 `class TMP_DialogueUI : DialogueUI`，并重写 `RunLine` 方法。然后全局替换原来的 DialogueUI 组件即可。
 
 ---
 
-Provide some new abilities for [YarnSpinner](https://yarnspinner.dev/).
+Add rich text ability for Yarn Spinner.
 
-Note: it's based on YarnSpinner v1.2 version.
+Both UI.Text and TextMeshPro support rich-text tags like HTML. But the official demo uses string split for typing effect, which breaks rich text display.
 
-branches:
-- `master`: the official demo project.
-- `saveload`: add save and load function when dialogue is running.
-- `richtext`: add rich text support.
+So we should use other solution: when a line start, set the whole text to the component, and change visible character count to achieve typing effect. In this example, we choose TextMeshPro because we can just set its `maxVisibleCharacters` property.
 
-Checkout branches to read the detailed documentation. 
+If you want to use the default UI.Text, you can also consider override the `OnPopulateMesh` method to limit the visible character count.
+
+In detail, we define `class TMP_DialogueUI : DialogueUI` and override `RunLine` method. Then replace the old DialogueUI component.
